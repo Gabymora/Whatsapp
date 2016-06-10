63011229961aa84c14bf98cb556b10297b90fa56
Gabymora <gtmorales.glez@gmail.com>
2016-06-10 14:44:04 -0500

    faltanmensajes

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
     //shows empty state text when no jobs found
     if( searchTerm === ' ') {
-      	$('.sub-menu').addClass('empty');
+        $('.sub-menu').addClass('empty');
     }
     else {
       $('.sub-menu').removeClass('empty');
@@ -48,31 +48,60 @@ $(document).ready(function() {
   });  
                     
 });
+//Buaca encuentra y remplaza
+$('.sub-menu').click(function() {
+  var data = $(this).attr('data')
+  $('.fondo-whatsapp').css({height: '435px',});
+  $('#message').css({display: 'block',});
+  var fotos = ["gian", "jose", "katy", "andrea", "aldo", "mariapaula", "raymi", "rodulfo", "anamaria"];
+  var nombres = ["Gian", "Jose", "Katy", "Andrea", "Aldo", "Maria Paula", "Raymi", "Rodulfo", "Ana Maria"];
+  $('#foto').html('<img class="media-object avatar" src="image/' +fotos[data]+ '.jpg" alt="...">');
+  $('#nombres').html('<p class= "nombre">' +nombres[data]+ '</p>');
+  $('#box-message').html('');
+});
+
+function textarea() {
+  var llamarID = document.getElementById("inputBox");
+  return llamarID.value;
+}
+$('body').keyup(function enter(e) {
+  var agregar = textarea();
+    if (agregar !== '') {
+      if(e.keyCode == 13) {       
+        actual();
+        ObtenerLista(agregar);
+      clean();
+          $('#inputBox').submit();
+      }
+    }
+});
+
 
 function ObtenerLista(text) {
-	var llamarDiv = document.getElementById("box-message");
-	
-	// Crear Elemento
-	var crearElemento = document.createElement("div");
-	crearElemento.id  = "box-green";
+  var llamarDiv = document.getElementById("box-message");
+  
+  // Crear Elemento
+  var crearElemento = document.createElement("div");
+  crearElemento.id  = "box-green";
 
-	var parrafo = document.createElement("div");
-	parrafo.id = "texto";
-	parrafo.innerHTML = text;
+  var parrafo = document.createElement("div");
+  parrafo.id = "texto";
+  parrafo.innerHTML = text;
 
-	var reloj = document.createElement("div");
-	reloj.id = "datatime"
-	reloj.innerHTML = times;
+  var reloj = document.createElement("div");
+  reloj.id = "datatime"
+/*  var times = actual();*/
+  reloj.innerHTML = times;
 
 
-	llamarDiv.appendChild(crearElemento);
-	crearElemento.appendChild(parrafo);
-	crearElemento.appendChild(reloj);
+  llamarDiv.appendChild(crearElemento);
+  crearElemento.appendChild(parrafo);
+  crearElemento.appendChild(reloj);
 
 }
 
 function clean() {
-	var llamarID =document.getElementById("inputBox");
-	inputBox.value = "";
-	inputBox.focus();
+  var llamarID =document.getElementById("inputBox");
+  inputBox.value = "";
+  inputBox.focus();
 }
